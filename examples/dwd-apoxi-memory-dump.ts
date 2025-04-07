51c6e4240d78d144a7e1d1d2f99ec3fd8c76cdb3
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2025-04-07 09:56:56 +0000

    memory dump

--- examples/dwd-apoxi-memory-dump.ts
+++ examples/dwd-apoxi-memory-dump.ts
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import { parseArgs } from 'node:util';
-import { sprintf } from 'sprintf-js';
-import { CGSN } from "../src/index.js";
+import { DWD } from "../src/index.js";
+import fs from "fs";
 import { openPort } from "./utils.js";
+import { parseArgs } from "node:util";
+import { sprintf } from "sprintf-js";
 
 const { values: argv } = parseArgs({
 	options: {
 		port: {
 			type: "string",
-			default: "/dev/ttyUSB0"
+			default: "/dev/ttyACM0"
 		},
 		addr: {
 			type: "string"
@@ -18,6 +18,10 @@ const { values: argv } = parseArgs({
 		},
 		out: {
 			type: "string"
+		},
+		key: {
+			type: "string",
+			default: "panasonic"
 		},
 		baudrate: {
 			type: "string",
@@ -37,7 +41,7 @@ const { values: argv } = parseArgs({
 
 const badParams = !argv.size || !argv.addr || !argv.out;
 if (badParams || argv.help || argv.usage) {
-	console.log(`USAGE: dump-ram.js --port /dev/ttyUSB0 --addr 0xA8000000 --size 0x1000000 --out /tmp/ram.bin`);
+	console.log(`USAGE: dwd-apoxi-memory-dump.js --port /dev/ttyACM0 --addr 0xB0000000 --size 0x1000000 --out /tmp/ram.bin`);
 	process.exit(0);
 }
 
@@ -54,25 +58,18 @@ if (isNaN(addr) || isNaN(size)) {
 
 const port = await openPort(argv.port, 115200);
 await port.open();
-const cgsn = new CGSN(port);
 
-if (await cgsn.connect()) {
-	console.log('Connected to CGSN!');
-} else {
-	console.log('Phone not found...');
-	process.exit(1);
-}
+const dwd = new DWD(port);
 
-await cgsn.setBestBaudRate(parseInt(argv.baudrate));
+dwd.setKeys(argv.key);
+await dwd.connect();
 
-const result = await cgsn.readMemory(addr, size, {
+const result = await dwd.readMemory(addr, size, {
 	onProgress({ percent, speed, remaining }) {
 		console.log(`Progress: ${percent.toFixed(2)}% | Speed: ${(speed / 1024).toFixed(2)} KB/s | ETA: ${remaining.toFixed(2)}s`);
 	}
 });
+fs.writeFileSync(argv.out!, result.buffer);
 
-if (result.success)
-	fs.writeFileSync(argv.out!, result.buffer);
-
-await cgsn.disconnect();
+await dwd.disconnect();
 await port.close();
